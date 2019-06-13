5a2ed3eee5e4154ec3319406bca63597372aba48
Tsegay <tsegay.tekle@gmail.com>
2019-06-13 16:10:27 -0400

    read toggle flag from properties file

--- gtas-parent/gtas-job-scheduler-war/src/main/java/gov/gtas/job/scheduler/RuleRunnerCondition.java
+++ gtas-parent/gtas-job-scheduler-war/src/main/java/gov/gtas/job/scheduler/RuleRunnerCondition.java
@@ -7,19 +7,26 @@ package gov.gtas.job.scheduler;
 
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Condition;
 import org.springframework.context.annotation.ConditionContext;
 import org.springframework.core.type.AnnotatedTypeMetadata;
+import org.springframework.stereotype.Component;
 
+@Component
 public class RuleRunnerCondition implements Condition {
 
-	private static final Logger logger  = LoggerFactory.getLogger(RuleRunnerCondition.class);
+	private static final Logger logger = LoggerFactory.getLogger(RuleRunnerCondition.class);
+
+	@Value("${enable.rule.runner}")
+	private String enableRuleRunner;
+
 	@Override
 	public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-		
-		logger.info("Rule runner is not running ");
-		
-		return true;
-	}
 
+		boolean enabled = Boolean.parseBoolean(this.enableRuleRunner);
+		logger.info("{} - Rule runner is {}running ", this.enableRuleRunner, enabled ? "" : "not ");
+
+		return enabled;
+	}
 }
